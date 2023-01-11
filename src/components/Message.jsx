6ab3dd2e324e74f7b8a2353ff4a212c2c9b4ecd5
Viviann1 <viviann.kodehode@gmail.com>
2023-01-11 18:33:05 +0100

    Finishing touches

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,27 +2,38 @@ import React, { useState } from "react";
 
   
 
-const YourMessage = () => {
+function YourMessage() {
     const [message, setMessage] = useState("");
-    console.log("Before log-timestamp");
-    console.log("After log-timestamp");
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const currentTime = new Date().toLocaleString();
+      const storedMessages =
+      JSON.parse(localStorage.getItem("messages")) || [];
+      const newMessages = [...storedMessages, { message, timestamp: currentTime }];
+      localStorage.setItem('messages', JSON.stringify(newMessages));
+      setMessage('');
+    }
+    console.log(message);
+    
+  
 
 
 
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Send a christmas message for the world to hear..</h2>
           <input className="messagefield"
           type="text"
           value={message}
-          onChange={(e) => setMessage(e.target.value)} 
+          onChange={e => setMessage(e.target.value)} 
           placeholder="My christmas message"
           aria-label="mymessage" 
           />
-          <input type="submit" value="Submit"></input>
+          <input className="submit" type="submit" value="Submit"></input>
       </form>
     
     );
-};
+}
 
       export default YourMessage;
